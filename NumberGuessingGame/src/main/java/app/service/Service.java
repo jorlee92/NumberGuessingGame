0f9d82b943cb5abeb6efa19cc65f3cfbaadaac84
AustinGraham28 <89654150+AustinGraham28@users.noreply.github.com>
2021-09-21 15:48:58 -0400

    updated service file

--- NumberGuessingGame/src/main/java/app/service/Service.java
+++ NumberGuessingGame/src/main/java/app/service/Service.java
@@ -3,7 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package app.service;
+package service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import models.Game;
+import models.Round;
 
 /**
  *
@@ -11,4 +18,38 @@ package app.service;
  */
 public class Service {
     
+    public String generateAnswer(){
+        
+        String result = "";
+        
+        for(int i = 0; i < 4; i++){
+            int random  = (int)((Math.random() * (10)));
+            
+            //if my result doesn't contain the new generated digit then I add it to the result
+            if(!result.contains(Integer.toString(random))){
+                result+=Integer.toString(random);
+            }
+        }
+        return result;    
+    }
+    
+    public String getRoundResult(String answer, String guess){
+        String result = "";
+        int exact = 0;
+        int partial = 0;
+        
+        for(int i = 0; i < 4; i++){
+            if(answer.charAt(i) == guess.charAt(i)){
+                exact++;
+            }
+            else{
+                for(int j = 0; j < 4; j++){
+                    if(answer.charAt(j) == guess.charAt(i)){
+                        partial++;
+                    }
+                }
+            }
+        }
+        return "e:"+exact+":p:"+partial;
+    }
 }
